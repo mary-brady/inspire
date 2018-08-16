e762c4d4b85072a331fcdb28797ca683909e123e
Mary Brady <emceebrady@gmail.com>
2018-08-16 14:13:02 -0600

    imported all to main.js

--- app/components/image/image-controller.js
+++ app/components/image/image-controller.js
@@ -1,3 +1,7 @@
+import ImageService from "./image-service.js"
+
+var imageService = new ImageService()
+
 //Your ImageService is a global class what can you do here to instantiate it?
 
 
--- app/main.js
+++ app/main.js
@@ -1,12 +1,19 @@
 import WeatherController from "./components/weather/weather-controller.js";
+import TodoController from "./components/todo/todo-controller.js";
+import QuoteController from "./components/quote/quote-controller.js";
+import ImageController from "./components/image/image-controller.js";
 
 // HEY WHERE ARE ALL OF YOUR CONTROLLERS??
 class app {
   constructor() {
     this.controllers = {
-      weatherController: new WeatherController()
+      weatherController: new WeatherController(),
+      toDoController: new TodoController(),
+      quoteService: new QuoteController(),
+      imageService: new ImageController()
     }
   }
 }
 
+// @ts-ignore
 window.app = new app()  
