668f7f67dbab2ae45e3177345df6b1f4ac9899de
Mary Brady <emceebrady@gmail.com>
2018-08-19 15:46:03 -0600

    some css + images changes

--- app/components/image/image-controller.js
+++ app/components/image/image-controller.js
@@ -4,7 +4,7 @@ var imageService = new ImageService()
 
 //Your ImageService is a global class what can you do here to instantiate it?
 function drawImage(image) {
-    let bodyDiv = document.getElementById('body');
+    let bodyDiv = document.getElementById('bg');
     bodyDiv.style.backgroundImage = "url(" + image.url + ")";
 }
 
